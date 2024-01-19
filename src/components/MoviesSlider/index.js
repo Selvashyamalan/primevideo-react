e2934082cd821fdacf9ec7600bfecf47f16a5141
selvashyamalan <selvamiitindia@gmail.com>
2024-01-19 12:47:57 +0000

    primevideo

---
+++ src/components/MoviesSlider/index.js
@@ -0,0 +1,27 @@
+import Slider from 'react-slick'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import MovieItem from '../MovieItem'
+
+const MovieSlider = props => {
+  const settings = {
+    dots: false,
+    slidesToScroll: 1,
+    slidesToShow: 4,
+  }
+
+  const {MoviesList} = props
+  return (
+    <>
+      <Slider {...settings}>
+        {MoviesList.map(eachMovie => (
+          <MovieItem key={eachMovie.id} movieDetails={eachMovie} />
+        ))}
+      </Slider>
+    </>
+  )
+}
+
+export default MovieSlider
